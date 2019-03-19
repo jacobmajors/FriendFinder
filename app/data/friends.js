aed4c00db9fdd50893bc083c33ab6cf541dbcc3e
Jacob Majors <jacobleemajors@gmail.com>
2019-03-19 04:31:51 -0500

    code works

---
+++ app/data/friends.js
@@ -0,0 +1,107 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+        name: "Larry David",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Larry_David_at_the_2009_Tribeca_Film_Festival_2.jpg/800px-Larry_David_at_the_2009_Tribeca_Film_Festival_2.jpg",
+        scores: [
+            "5",
+            "5",
+            "2",
+            "5",
+            "1",
+            "1",
+            "1",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Cheryl David",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Cheryl_Hines_2011_AA.jpg/800px-Cheryl_Hines_2011_AA.jpg",
+        scores: [
+            "1",
+            "3",
+            "4",
+            "3",
+            "5",
+            "5",
+            "5",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Jeff Greene",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Garlin.jpg/800px-Garlin.jpg",
+        scores: [
+            "5",
+            "4",
+            "3",
+            "4",
+            "4",
+            "2",
+            "1",
+            "3",
+            "5",
+            "4"
+        ]
+    },
+    {
+        name: "Richard Lewis",
+        photo: "https://www.gilbertpodcast.com/wp-content/uploads/2017/09/richlewis.jpg",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "5",
+            "4",
+            "3",
+            "2",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Susie Greene",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Susie_Essman_at_the_2009_Tribeca_Film_Festival.jpg/800px-Susie_Essman_at_the_2009_Tribeca_Film_Festival.jpg",
+        scores: [
+            "5",
+            "1",
+            "2",
+            "1",
+            "3",
+            "5",
+            "5",
+            "5",
+            "2",
+            "4"
+        ]
+    },
+    {
+        name: "Marty Funkhouser",
+        photo: "http://goodnightsweetprince.rip/s/miles-klee/funkhouser_portrait.jpg",
+        scores: [
+            "5",
+            "2",
+            "3",
+            "1",
+            "1",
+            "5",
+            "5",
+            "4",
+            "1",
+            "3"
+        ]
+    }
+
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,65 @@
+// Require path
+
+var path = require("path");
+
+// Import friends
+var friends = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+    // API GET Requests
+    // List of friends
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    // Add new friend
+    app.post("/api/friends", function (req, res) {
+        // Best friend match
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
+        };
+
+        // Takes user data object from survey
+        var userData = req.body;
+        // User scores combined with body
+        var userScores = userData.scores;
+
+        // Variable to hold difference in scores
+        var totalDifference;
+
+        // Loop through all of the friends
+        for (var i = 0; i < friends.length; i++) {
+            var currentFriend = friends[i];
+            totalDifference = 0;
+
+            // Loop through each friend's scores
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = userScores[j];
+
+                // Calculate the difference between the scores and sum them into the totalDifference
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+            };
+
+            // If the sum of differences is less than the differences of the current best match
+            if (totalDifference <= bestMatch.friendDifference) {
+                // Reset the bestMatch to be the new friend
+                bestMatch.name = currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.friendDifference = totalDifference;
+            };
+        };
+
+        // Add new user to friend database
+        friends.push(userData);
+
+        // Send response
+        res.json(bestMatch);
+    });
+};
